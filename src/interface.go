4c1df5d57538b6471187cd8f797bb0356424ad2d
ravinu <github@swore3556.anonaddy.me>
2024-10-21 21:31:14 +0530

    Fix for merge conflicts

--- src/interface.go
+++ src/interface.go
@@ -2,30 +2,35 @@ package main
 
 import "fmt"
 
-//import "fmt"
-
 func Interface() {
+	NewOption := 0
 	ScreenClear(0)
 	for {
 		msg("Select a option:")
 		msg("1) Add task\n2) View due tasks\n3) Archived tasks")
-		fmt.Scan(&option)
-		if option == 1 {
+		fmt.Scan(&NewOption)
+		switch NewOption {
+		case 1:
 			status_check()
-			msg("Under construction")
-			ScreenClear(1)
-			break
-		} else if option == 2 {
 			ScreenClear(0)
 			msg("Under construction")
 			ScreenClear(1)
 			break
-		} else if option == 3 {
+		case 2:
 			ScreenClear(0)
+			fmt.Println("Working")
 			msg("Under construction")
 			ScreenClear(1)
-		} else {
+			break
+		case 3:
 			ScreenClear(0)
+			fmt.Println("Working")
+			msg("Under construction")
+			ScreenClear(1)
+			break
+		default:
+			ScreenClear(0)
+			fmt.Println("Working")
 			msg("Error try again...")
 			ScreenClear(1)
 		}
