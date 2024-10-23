171328bdf954a9b2ae92cb1ea322dcb03b92c89c
ravinu <github@swore3556.anonaddy.me>
2024-10-23 20:59:10 +0530

    Impliemnt 'Enter' to exit and transition to new ScreenClear func

--- src/interface.go
+++ src/interface.go
@@ -7,24 +7,26 @@ import (
 
 func Interface() {
 	ScreenClear(0)
+	var option string
 	for {
+		option = ""
 		fmt.Println("Select a option:")
-		fmt.Println("\t1) Add task\n\t2) View due tasks\n\t3) Archived tasks")
+		fmt.Println("\t1) Add task\n\t2) View due tasks\n\t3) Archived tasks\nPress enter to exit")
 		fmt.Print(">>")
-		fmt.Scan(&option)
+		fmt.Scanln(&option)
 		switch option {
-		case 1:
+		case "1":
 			ScreenClear(0)
 			status_check()
 			//text := parse("upcoming.txt")
 			//fmt.Println(text)
 			break
-		case 2:
+		case "2":
 			ScreenClear(0)
 			msg("Under construction")
-			ScreenClear(1)
+			ScreenClear(300)
 			fmt.Println("Reading \"Upcoming.txt\"")
-			ScreenClear(0)
+			ScreenClear(100)
 			text := parse("upcoming.txt")
 			for i := 0; i < len(text); i++ {
 				fmt.Printf("Task %d: %s\n", i+1, strings.Join(text[i], " "))
@@ -33,15 +35,18 @@ func Interface() {
 			fmt.Scanln()
 			ScreenClear(0)
 			break
-		case 3:
+		case "3":
 			ScreenClear(0)
 			msg("Still working on it")
-			ScreenClear(1)
+			ScreenClear(600)
 			break
+		case "":
+			println("Exiting...")
+			return
 		default:
 			ScreenClear(0)
 			msg("Error try again...")
-			ScreenClear(1)
+			ScreenClear(700)
 		}
 	}
 }
