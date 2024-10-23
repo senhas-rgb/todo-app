b1963b32bdb836578b5e6f65a2a6e4d383ad2ebd
ravinu <github@swore3556.anonaddy.me>
2024-10-23 20:36:58 +0530

    Add task printing functionality and clean up interface

--- src/interface.go
+++ src/interface.go
@@ -1,24 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Interface() {
 	ScreenClear(0)
 	for {
-		msg("Select a option:")
-		msg("1) Add task\n2) View due tasks\n3) Archived tasks")
+		fmt.Println("Select a option:")
+		fmt.Println("\t1) Add task\n\t2) View due tasks\n\t3) Archived tasks")
+		fmt.Print(">>")
 		fmt.Scan(&option)
 		switch option {
 		case 1:
 			ScreenClear(0)
 			status_check()
-			text := parse("upcoming.txt")
-			fmt.Println(text)
+			//text := parse("upcoming.txt")
+			//fmt.Println(text)
 			break
 		case 2:
 			ScreenClear(0)
 			msg("Under construction")
 			ScreenClear(1)
+			fmt.Println("Reading \"Upcoming.txt\"")
+			ScreenClear(0)
+			text := parse("upcoming.txt")
+			for i := 0; i < len(text); i++ {
+				fmt.Printf("Task %d: %s\n", i+1, strings.Join(text[i], " "))
+			}
+			fmt.Printf("\nPress enter to exit")
+			fmt.Scanln()
+			ScreenClear(0)
 			break
 		case 3:
 			ScreenClear(0)
